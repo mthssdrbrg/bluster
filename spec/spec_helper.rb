d16985a8e7d130f6b56d69b5a017d1969d380bc9
Mathias Söderberg <mathias.soederberg@gmail.com>
2013-08-23 01:26:00 +0200

    Add simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
-require 'bluster'
+# encoding: utf-8
+
+require 'simplecov'
+SimpleCov.start
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.order = 'random'
 end
+
+require 'bluster'
